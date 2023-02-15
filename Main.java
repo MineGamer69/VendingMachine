43a7800702c3178e06d894c380c1dee2b0751237
MineGamer69 <aaryankapoor2003@gmail.com>
2023-02-14 19:46:39 -0500

    Added strings and input statemnets

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Welcome to the Vending Machine");
+        System.out.println("1) Chips: $2\n2) M&M's: $1.8\n3) Skittles: $2\n4) Water: $1\nPlease Enter a snack Number: ");
+        int option = inp.nextInt();
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# VendingMachine
+CSC109
