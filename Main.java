c262a358e29b6456274d966ff5a690a6d2443183
github user name <github email>
2023-02-14 20:19:34 -0500

    Added input statement for number of  bills

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main{
         System.out.println("Welcome to the Vending Machine");
         System.out.println("1) Chips: $2\n2) M&M's: $1.8\n3) Skittles: $2\n4) Water: $1\nPlease Enter a snack Number: ");
         int option = inp.nextInt();
+        System.out.println("Please enter number of $1 bills you will be using: ");
+        int dollarBills = inp.nextInt();
+        //Next: print the user's change
     }
 
 
