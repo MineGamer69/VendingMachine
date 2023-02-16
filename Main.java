094c6c2b68cada2f1eb02fd49ff879e8f360dc22
github user name <github email>
2023-02-15 22:43:07 -0500

    Added confirmation option and a refund variable

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Main{
+    private static double possibleRefund;
     public static void main(String[] args){
         Scanner inp = new Scanner(System.in);
         System.out.println("Welcome to the Vending Machine");
@@ -10,23 +11,47 @@ public class Main{
         if(option == 1){
             double change1 = dollarBills - 2;
             System.out.println("Change returned: " + change1);
+            possibleRefund = 2.0;
         }
         else if(option == 2){
             double change2 = dollarBills - 1.8;
             System.out.println("Change returned: " + change2);
+            possibleRefund = 1.8;
         }
         else if (option == 3){
             double change3 = dollarBills - 2.5;
             System.out.println("Change returned: " + change3);
+            possibleRefund = 2.5;
         }
         else if (option == 4){
             double change4 = dollarBills - 1;
             System.out.println("Change returned: " + change4);
+            possibleRefund = 1.0;
+        } 
+
+        System.out.println("Would you like to confirm the transaction?(1 for yes, 2 for no): ");
+        int confirmationOfOrder;
+        confirmationOfOrder = inp.nextInt();
+        if (confirmationOfOrder == 1 ) {
+            System.out.println("Thank you for your purchase, have a nice day!");
         }
-        /*The user is asked for confirmation on the transaction before it is carried out, and if the user
-decides to cancel the transaction, they are given a full refund.
-• The user is given a departure message that makes them feel good about their snack choice. */
+        else if (confirmationOfOrder == 2 ) {
+            System.out.println("Your total refund is: $" + possibleRefund + ". Have a nice day!");
+        }
+        else {
+            while(confirmationOfOrder != 1 || confirmationOfOrder != 2) {
+                System.out.println("Error, Please Enter a valid number(1 to confirm your purchase, 2 for a refund): ");
+                confirmationOfOrder = inp.nextInt();
+                if (confirmationOfOrder == 1 ) {
+                    System.out.println("Thank you for your purchase, have a nice day!");
+                    break;
+                }
+                else if (confirmationOfOrder == 2 ) {
+                    System.out.println("Your total refund is: $" + possibleRefund + ". Have a nice day!");
+                    break;
+                }
+            }
+        }
+        
     }
-
-
 }
